5b3b878b931a8738bb34e599efb45d57cc564ab1
sosankit07 <36675268+sosankit07@users.noreply.github.com>
2018-03-02 00:01:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get('/three', function (req, res) {
 var counter = 0;
 app.get('/counter', function (req, res) {
   counter = counter + 1;
-  res.send(counter);
+  res.send(counter,'toString()');
 });
 
 
