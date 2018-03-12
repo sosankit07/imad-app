74f4c3e978606332ea1486600408af4625cc0cc4
sosankit07 <36675268+sosankit07@users.noreply.github.com>
2018-03-13 00:48:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,20 @@
 console.log("Loaded");
-var button = document.getElementById("counter");
+var button=document.getElementById("counter");
+
 button.onclick=function(){
-    //create a request
-    var request=new XMLHttpRequest();
-    request.onreadystatechange = function(){
-        if(request.readystate === XMLHttpRequest.DONE){
-            if(request.status === 200){
-                var counter = request.responseText;
-                var span = document.getElementById("count");
-                span.innerHTML = counter.toString();
-            }
+
+var request=new XMLHttpRequest();
+request.onreadystatechange=function(){
+    if(request.readyState===XMLHttpRequest.DONE)
+    {
+        if(request.status===200){
+            var counter=request.responseText;
+            var span=document.getElementById("count");
+            span.innerHTML=counter.toString();
         }
-        
-    };
-    //make a request
-    request.open("GET","http://dubeankit07.imad.hasura-app.io/counter",true);
-    request.send(null);
+    }
+};
+
+request.open('GET','http://http://dubeankit07.imad.hasura-app.io/counter',true);
+request.send(null);
 };
