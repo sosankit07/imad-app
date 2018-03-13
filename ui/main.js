9dca2b583f9fa2eee38006cb4243b18f2f46a7b6
Ankit Dubey <36675268+sosankit07@users.noreply.github.com>
2018-03-13 21:10:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,7 +27,7 @@ request.onreadystatechange=function(){
     if(request.readyState===XMLHttpRequest.DONE)
     {
         if(request.status===200){
-         var names=[];
+         var names=["name1","name2","name3","name4"];
             var list=' ';
           for(var i=0;i<names.length;i++){
             list='<li>'+names[i]+'</li>';
