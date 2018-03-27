ae0fb00be31d5e49ba569d6975575d8d4a1e9ac4
Ankit Dubey <36675268+sosankit07@users.noreply.github.com>
2018-03-27 16:58:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ var app = express();
 app.use(morgan('combined'));
 
 function hash(input,salt){
-    var hashed = crypto.pbkd2Sync(input,salt,10000,512,'sha512');
+    var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
     return(hashed.toString("hex"));
 }
 app.get('/hash/:input',function(req,res){
